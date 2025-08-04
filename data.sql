b63b0485a7ed8bed49cc7bd9f16c322403f43f5f
Baskar <baskar19erbt@outlook.com>
2025-08-04 19:38:27 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,7 +1,8 @@
--- Insert Inventory Items
-INSERT INTO Inventory VALUES (1, 'Sensor', 50);
-INSERT INTO Inventory VALUES (2, 'Resistor Pack', 200);
+insert into departments values (281,'Finance','France');
 
--- Insert Customers
-INSERT INTO Customers VALUES (101, 'Arun', 'arun@mail.com');
-INSERT INTO Customers VALUES (102, 'Divya', 'divya@mail.com');
+insert into departments values (282,'Salse','France');
+
+insert into employees values (402,'Mohan','Junior Developer',sysdate,500,281);
+insert into employees values (403,'Riya','Junior Developer',sysdate,500,281);
+insert into employees values (404,'Ajay','Junior Developer',sysdate,500,282);
+insert into employees values (405,'Mark','Junior Developer',sysdate,500,282);
